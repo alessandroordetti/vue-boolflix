b4ed820f5418e3157a3f87b34a16344356023fd3
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-01 13:51:29 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IndexHeader />
+
+    <IndexMain />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexHeader from './components/IndexHeader.vue';
+import IndexMain from './components/IndexMain.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IndexHeader,
+    IndexMain
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/style/main-style.scss';
+
 </style>
---
+++ src/components/IndexHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="navbar navbar-dark justify-content-between">
+                        <img src="../assets/logo.png" alt="Netflix Logo">
+                        <form class="form-inline d-flex">
+                            <input class="form-control mx-3 mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'IndexHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        background-color: black;
+        height: 10vh;
+    }
+
+    nav {
+        height: 10vh;
+
+        img {
+            height: 100%;
+        }
+    }
+
+
+</style>
