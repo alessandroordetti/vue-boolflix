4709de38465f5e04b8a01e85fd37616906a2fcf6
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-01 15:58:06 +0200

    First Milestone Completed

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="card p-2 text-center">
+            <div class="card-body">
+                <h5 class="card-title">{{titolo}}</h5>
+                <p class="card-text">{{titoloOriginale}}</p>
+                <p class="card-text">{{lingua}}</p>
+                <p class="card-text">{{votoMedio}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FilmCard',
+
+    props: ['lingua', 'titolo', 'titoloOriginale', 'votoMedio'],
+}
+
+</script>
+
+<style>
+
+</style>
--- src/components/IndexMain.vue
+++ src/components/IndexMain.vue
@@ -1,12 +1,57 @@
 <template>
     <main>
-
+        <div class="container">
+            <div class="row">
+                <div class="" v-for="(element, index) in filmList" :key=index>
+                    <FilmCard 
+                        :lingua="element.original_language"
+                        :titolo="element.original_title"
+                        :titoloOriginale="element.original_title"
+                        :votoMedio="element.vote_average"
+                    />
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
+import axios from "axios";
+import FilmCard from './FilmCard.vue'
+
+
+
 export default {
     name: 'IndexMain',
+
+        components: {
+        FilmCard,
+    },
+
+
+    data: function(){
+        return {
+            filmList: '',
+        }
+    },
+
+    created: function () {
+    this.getApi();
+    },
+
+    methods: {
+    getApi () {
+        axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=25cf02f7f319fef92585c5875256324a&language=en-US&page=1&include_adult=false&query=1')
+        .then((result)=> {
+        this.filmList = result.data.results;
+        })
+        .catch((error) =>{
+        console.error(error);
+        })
+        }
+    },
+
 }
 </script>
 
